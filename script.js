c213440bfa48e17e716f6e946a29b6ed42d732b2
JaceVin <denzelhdez09@gmail.com>
2023-12-03 20:19:49 -0500

    Temporary Message on Caption
    / +
    | setup extracted function that
    | asigns a message to element
    | and reverts to original text after delay
    /!/
    / +
    | display temporary message
    | when #promo-code-caption is on click
    / /
    
    v0.2.3.11

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const promoCodeCaption = document.querySelector('#promo-code-caption');
 
-promoCodeCaption.addEventListener('click', event => {});
+promoCodeCaption.addEventListener('click', event => {
+    temporaryMessageOn(promoCodeCaption, 'Copied!', 850);
+});
+
+function temporaryMessageOn(element, message, delay) {
+    const originalText = element.innerText;
+    element.innerText = message;
+    setTimeout( () => {
+        element.innerText = originalText;
+    }, delay);
+}
 
