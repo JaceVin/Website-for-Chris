149496806ca33bb3d21ef1f7ca51b0e9c313d3d8
JaceVin <denzelhdez09@gmail.com>
2023-12-08 16:17:24 -0500

    sad

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const promoCodeCaption = document.querySelector('#promo-code-caption');
+const promoCode = document.querySelector('#promo-code');
 
-promoCodeCaption.addEventListener('click', event => {
-    const promoCode = document.querySelector('#promo-code');
+const prevViewportWidth = window.innerWidth;
+
+['DOMContentLoaded', 'resize'].forEach(eventType => {
+    window.addEventListener(eventType, resizePromoCode)
+})
+
+promoCodeCaption.addEventListener('click', () => {
     navigator.clipboard.writeText(promoCode.innerText);
 
     temporaryMessageOn(promoCodeCaption, 'Copied!', 850);
@@ -17,4 +23,23 @@ function temporaryMessageOn(element, message, delay) {
         element.innerText = originalText;
     }, delay);
 }
+
+function resizePromoCode() {
+    const gridWidth = promoCode.parentElement.scrollWidth;
+    // const maxWidth = document.body.scrollWidth;
+    
+    // const minFontSize = 8;
+    const maxFontSize = 48;
+    const currentFontSize = getComputedStyle(promoCode).fontSize;
+    
+    let newFontSize = Number(currentFontSize.replace('px', ''));
+    
+    while (
+        promoCode.scrollWidth < gridWidth &&
+        newFontSize != maxFontSize
+    ) promoCode.style.fontSize = (newFontSize += 2) + 'px';
+
+    // resize code
+}
+// new ResizeObserver(resizePromoCode).observe(promoCode);
 
